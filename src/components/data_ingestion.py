c087707f3e2a48410ae18b7f1f3eddc324af2496
Rithish S <123345316+Rithish5513U@users.noreply.github.com>
2024-07-06 10:03:11 +0530

    Add files via upload

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestion:
+    credits_path: str = 'E:/Krish Naik/notebooks/data/tmdb_5000_credits.csv'
+    movies_path: str = 'E:/Krish Naik/notebooks/data/tmdb_5000_movies.csv'
+
+    def data_ingestion(self):
+        logging.info('Data Ingestion started')
+        try:
+            credits = pd.read_csv(self.credits_path)
+            movies = pd.read_csv(self.movies_path)
+            logging.info('Data Ingestion completed')
+            return credits, movies
+        except Exception as e:
+            logging.error('Error during data ingestion')
+            raise CustomException(e)
+        
+obj = DataIngestion()
+obj.data_ingestion()
